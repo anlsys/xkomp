48500cbfae4bf1eabddb240310001c8526426bf0
Romain PEREIRA <rpereira@anl.gov>
2025-07-18 22:57:00 +0000

    Added missing barrier

--- src/barrier.cc
+++ src/barrier.cc
@@ -7,11 +7,13 @@ __kmpc_barrier(
     ident_t * loc,
     kmp_int32 global_tid
 ) {
-    # if 0
     xkomp_t * xkomp = xkomp_get();
     xkrt_thread_t * thread = xkrt_thread_t::get_tls();
     assert(thread);
 
+    # if 0
+    xkomp->runtime.task_wait();
+    #else
     xkomp->runtime.team_barrier<true>(thread->team, thread);
     # endif
 }
