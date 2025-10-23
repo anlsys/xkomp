5b2003b832b344e1c479a1eb777dc9ae4935bb61
PEREIRA Romain <romain.pereira@outlook.com>
2025-10-23 00:28:14 -0500

    Update depend.cc

--- interfaces/interop/depend.cc
+++ interfaces/interop/depend.cc
@@ -2,35 +2,6 @@
 // Do we want to synchronize ? //
 /////////////////////////////////
 
-// 0'
-
-int x[1];
-x[0] = 0;
-
-# pragma omp enter data map(storage: x[0:1]) device(0)
-# pragma omp enter data map(storage: x[0:1]) device(1)
-
-# pragma omp target nowait update to(x[0:1]) depend(out: x)
-
-# pragma omp target nowait device(0) depend(out: x)
-    x = 42;
-
-omp_depend_t deps[] = {
-    ...
-};
-
-void * src = omp_get_mapped_ptr(x, 0);
-void * dst = omp_get_mapped_ptr(x, 1);
-omp_target_memcpy_async(dst, src, ..., deps, sizeof(deps)/sizeof(omp_depend_t));
-
-# pragma omp target nowait device(1) depend(in: x)
-    puts(x);
-
-# pragma omp taskwait
-
-puts(x)
-
-
 // 0
 
 x = 0;
